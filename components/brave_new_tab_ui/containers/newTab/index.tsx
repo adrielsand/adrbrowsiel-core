a09237077135c3694396c5f3ff5d4022610cb786
Simon Hong <shong@brave.com>
2021-04-26 10:40:36 +0900

    Address comments
    * Changed to use replacement for translation
    * Deleted redunt methods
    * Changed to cancel order when count is ended in order confirm screen

--- components/brave_new_tab_ui/containers/newTab/index.tsx
+++ components/brave_new_tab_ui/containers/newTab/index.tsx
@@ -329,10 +329,6 @@ class NewTabPage extends React.Component<Props, State> {
     this.props.actions.setGeminiHideBalance(hide)
   }
 
-  setCryptoDotComHideBalance = (hide: boolean) => {
-    this.props.actions.setCryptoDotComHideBalance(hide)
-  }
-
   disconnectBinance = () => {
     this.props.actions.disconnectBinance()
   }
@@ -1037,7 +1033,7 @@ class NewTabPage extends React.Component<Props, State> {
         onDisableWidget={this.toggleShowCryptoDotCom}
         onBtcPriceOptIn={this.props.actions.onBtcPriceOptIn}
         onIsConnected={this.props.actions.onIsConnectedReceived}
-        onSetHideBalance={this.setCryptoDotComHideBalance}
+        onSetHideBalance={this.props.actions.setCryptoDotComHideBalance}
         disconnect={this.disconnectCryptoDotCom}
         cancelDisconnect={this.cancelCryptoDotComDisconnect}
         isCryptoDotComLoggedIn={chrome.cryptoDotCom.isLoggedIn}
